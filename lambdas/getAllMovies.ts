5ef6152c334792333759c3c55eebd13eed5300e1
lumoumou123 <20109114@mail.wit.ie>
2025-02-15 18:26:33 +0000

    Basic REST web API

--- lambdas/getAllMovies.ts
+++ lambdas/getAllMovies.ts
@@ -1,11 +1,10 @@
-import { Handler } from "aws-lambda";
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 
-const ddbDocClient = createDDbDocClient();
-
-export const handler: Handler = async (event, context) => {
+const ddbDocClient = new DynamoDBClient({ region: process.env.REGION });
+export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
   try {
     // Print Event
     console.log("Event: ", event);
