303ceba1109b6d8e3261e835ad6cdd8228fef75d
lumoumou123 <20109114@mail.wit.ie>
2025-02-16 12:11:42 +0000

    Exercise: Expand get movie endpoint

--- lambdas/getMovieById.ts
+++ lambdas/getMovieById.ts
@@ -2,17 +2,39 @@ import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 
 
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, GetCommand ,QueryCommand} from "@aws-sdk/lib-dynamodb";
 
 const ddbDocClient = createDDbDocClient();
+const docClient = DynamoDBDocumentClient.from(ddbDocClient);
+// 定义响应的类型
+interface MovieResponse {
+  data: Record<string, any> | undefined;  // 允许 data 是 undefined
+  cast?: Record<string, any>[];  // 演员信息，可选
+}
+async function getMovieDetails(movieId: number) {
+    const commandOutput = await docClient.send(new GetCommand({
+        TableName: process.env.TABLE_NAME,
+        Key: { id: movieId },
+    }));
+    return commandOutput.Item;
+}
+
+async function getCastDetails(movieId: number) {
+    const commandOutput = await docClient.send(new QueryCommand({
+        TableName: process.env.CAST_TABLE_NAME,
+        KeyConditionExpression: "movieId = :movieId",
+        ExpressionAttributeValues: { ":movieId": movieId },
+    }));
+    return commandOutput.Items;
+}
 
 export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
   try {
     
     // Print Event
     console.log("[EVENT]", JSON.stringify(event));
-    const parameters  = event?.pathParameters;
-    const movieId = parameters?.movieId ? parseInt(parameters.movieId) : undefined;
+    const movieId = event.pathParameters?.movieId ? parseInt(event.pathParameters.movieId) : undefined;
+    const includeCast = event.queryStringParameters?.cast === 'true';
 
     if (!movieId) {
       return {
@@ -23,26 +45,20 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
         body: JSON.stringify({ Message: "Missing movie Id" }),
       };
     }
-
-    const commandOutput = await ddbDocClient.send(
-      new GetCommand({
-        TableName: process.env.TABLE_NAME,
-        Key: { id: movieId },
-      })
-    );
-    console.log("GetCommand response: ", commandOutput);
-    if (!commandOutput.Item) {
+    const movieDetails = await getMovieDetails(movieId);
+    if (!movieDetails) {
       return {
         statusCode: 404,
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({ Message: "Invalid movie Id" }),
+        body: JSON.stringify({ message: "Movie not found" }),
       };
     }
-    const body = {
-      data: commandOutput.Item,
-    };
+    const response: MovieResponse = { data: movieDetails };
+        if (includeCast) {
+          const castDetails = await getCastDetails(movieId);
+          if (castDetails) {
+              response.cast = castDetails; // 使用 MovieResponse 接口中的可选 cast 属性
+          }
+        }
 
     // Return Response
     return {
@@ -50,7 +66,7 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
       headers: {
         "content-type": "application/json",
       },
-      body: JSON.stringify(body),
+      body: JSON.stringify(response),
     };
   } catch (error: any) {
     console.log(JSON.stringify(error));
--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -6,7 +6,7 @@ import * as custom from "aws-cdk-lib/custom-resources";
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { generateBatch } from "../shared/util";
-import { movies } from "../seed/movies";
+import { movies, movieCasts } from "../seed/movies";
 import * as apig from "aws-cdk-lib/aws-apigateway";
 
 export class RestAPIStack extends cdk.Stack {
@@ -20,6 +20,19 @@ export class RestAPIStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       tableName: "Movies",
     });
+    const movieCastsTable = new dynamodb.Table(this, "MovieCastTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: "movieId", type: dynamodb.AttributeType.NUMBER },
+      sortKey: { name: "actorName", type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: "MovieCast",
+    });
+
+    movieCastsTable.addLocalSecondaryIndex({
+      indexName: "roleIx",
+      sortKey: { name: "roleName", type: dynamodb.AttributeType.STRING },
+    });
+
 
 
     // Functions 
@@ -34,6 +47,7 @@ export class RestAPIStack extends cdk.Stack {
         memorySize: 128,
         environment: {
           TABLE_NAME: moviesTable.tableName,
+          CAST_TABLE_NAME:movieCastsTable.tableName,
           REGION: 'eu-west-1',
         },
       }
@@ -79,7 +93,21 @@ export class RestAPIStack extends cdk.Stack {
       },
     });
 
-
+    const getMovieCastMembersFn = new lambdanode.NodejsFunction(
+      this,
+      "GetCastMemberFn",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_16_X,
+        entry: `${__dirname}/../lambdas/getMovieCastMember.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: movieCastsTable.tableName,
+          REGION: "eu-west-1",
+        },
+      }
+    );
 
     new custom.AwsCustomResource(this, "moviesddbInitData", {
       onCreate: {
@@ -88,21 +116,24 @@ export class RestAPIStack extends cdk.Stack {
         parameters: {
           RequestItems: {
             [moviesTable.tableName]: generateBatch(movies),
+            [movieCastsTable.tableName]: generateBatch(movieCasts),  // Added
           },
         },
         physicalResourceId: custom.PhysicalResourceId.of("moviesddbInitData"), //.of(Date.now().toString()),
       },
       policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
-        resources: [moviesTable.tableArn],
+        resources: [moviesTable.tableArn, movieCastsTable.tableArn],  // Includes movie cast
       }),
     });
+
 
     // Permissions 
     moviesTable.grantReadData(getMovieByIdFn)
     moviesTable.grantReadData(getAllMoviesFn)
     moviesTable.grantReadWriteData(newMovieFn)
     moviesTable.grantReadWriteData(deleteMovieFn);
-
+    movieCastsTable.grantReadData(getMovieCastMembersFn);
+    movieCastsTable.grantReadData(getMovieByIdFn);
 
     const api = new apig.RestApi(this, "RestAPI", {
       description: "demo api",
@@ -126,6 +157,7 @@ export class RestAPIStack extends cdk.Stack {
       "POST",
       new apig.LambdaIntegration(newMovieFn, { proxy: true })
     );
+
     const movieEndpoint = moviesEndpoint.addResource("{movieId}");
     movieEndpoint.addMethod(
       "GET",
@@ -135,8 +167,15 @@ export class RestAPIStack extends cdk.Stack {
       "DELETE",
       new apig.LambdaIntegration(deleteMovieFn, { proxy: true })
     );
-    
-    
+
+    const movieCastEndpoint = moviesEndpoint.addResource("cast");
+    movieCastEndpoint.addMethod(
+      "GET",
+      new apig.LambdaIntegration(getMovieCastMembersFn, { proxy: true })
+    );
+
+
+
 
   }
 }
